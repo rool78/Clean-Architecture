2d89a8544041a29eb921dc4756786678db2550eb
rooL <raul.mazaa@gmail.com>
2021-10-08 16:53:53 +0200

    .

--- app/src/main/java/com/example/universityutils/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/universityutils/ui/notes/NotesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.universityutils.ui.notifications
+package com.example.universityutils.ui.notes
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.universityutils.R
-import com.example.universityutils.databinding.FragmentNotificationsBinding
+import com.example.universityutils.databinding.FragmentNotesBinding
 
-class NotificationsFragment : Fragment() {
+class NotesFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var notesViewModel: NotesViewModel
+    private var _binding: FragmentNotesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        notesViewModel =
+            ViewModelProvider(this).get(NotesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textNotes
+        notesViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/universityutils/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/example/universityutils/ui/notes/NotesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.universityutils.ui.notifications
+package com.example.universityutils.ui.notes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class NotesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is notes Fragment"
     }
     val text: LiveData<String> = _text
 }
