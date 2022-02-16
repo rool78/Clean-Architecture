187807d375f9ade1e41aa7ab09796dde0dc12ddf
rooL <raul.mazaa@gmail.com>
2022-02-16 23:31:30 +0100

    .

--- app/src/main/java/com/example/universityutils/features/food/presentation/adapter/FoodRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/universityutils/features/food/presentation/adapter/FoodRecyclerViewAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.universityutils.features.food.presentation.adapter
 
 import android.content.Context
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,20 +10,14 @@ import com.example.universityutils.R
 import com.example.universityutils.features.food.domain.model.Food
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
+import com.squareup.picasso.Picasso
+
 
 class FoodRecyclerViewAdapter(
     private val foodList: MutableList<Food>,
     private val context: Context
 ) :
     RecyclerView.Adapter<FoodRecyclerViewAdapter.CustomViewHolder>() {
-
-    fun removeNote(food: Food) {
-        val index = foodList.indexOf(food)
-        if (index != -1) {
-            foodList.removeAt(index)
-            notifyItemRemoved(index)
-        }
-    }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         println("onBindViewHolder: ${foodList[position]}")
@@ -35,7 +28,7 @@ class FoodRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         return CustomViewHolder(
             LayoutInflater.from(parent.context).inflate(
-                R.layout.note_card_view,
+                R.layout.food_card_view,
                 parent,
                 false
             )
@@ -48,23 +41,14 @@ class FoodRecyclerViewAdapter(
 
         fun bind(food: Food, context: Context) {
             println("viewHolder bind -> $food")
-            itemView.findViewById<MaterialCardView>(R.id.note_card).setOnClickListener {
-                println("view Note")
+            itemView.findViewById<MaterialCardView>(R.id.food_card).setOnClickListener {
+                println("#### food card clicked...$it")
             }
-            if (food.imageUrl != null) {
-//                itemView.findViewById<Material>(R.id.food_image).setImageURI(Uri.parse(food.imageUrl))
-            }
+            val image = itemView.findViewById<ImageView>(R.id.food_image)
+            Picasso.get().load(food.imageUrl).into(image)
             itemView.findViewById<MaterialTextView>(R.id.tv_food_name).text = food.name
-            itemView.findViewById<MaterialTextView>(R.id.tv_food_calories).text = food.caloriesPer100g.toString()
-
-            //TODO date
-//            itemView.findViewById<MaterialButton>(R.id.remove_post_button).setOnClickListener {
-//                removePost(note)
-//                Toast.makeText(context, "Removing note", Toast.LENGTH_SHORT).show()
-//            }
-//            itemView.findViewById<MaterialButton>(R.id.edit_note_button).setOnClickListener {
-//                Toast.makeText(context, "Edit note", Toast.LENGTH_SHORT).show()
-//            }
+            itemView.findViewById<MaterialTextView>(R.id.tv_food_calories).text =
+                food.caloriesPer100g.toString()
         }
     }
 }
