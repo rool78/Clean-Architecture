b4aa14ef77ec1d4db91308dbdb6f232f5f4fec8d
rool78 <raul.mazaa@gmail.com>
2022-04-02 23:19:47 +0200

    Add different DI modules

--- app/src/main/java/com/example/universityutils/di/AppModule.kt
+++ app/src/main/java/com/example/universityutils/di/AppModule.kt
@@ -1,29 +1,12 @@
 package com.example.universityutils.di
 
-import android.content.Context
-import androidx.room.Room
-import com.example.universityutils.features.food.data.remote.OpenFoodApi
-import com.example.universityutils.features.food.data.repository.FoodRepositoryImpl
-import com.example.universityutils.features.food.domain.repository.FoodRepository
-import com.example.universityutils.features.food.domain.use_case.SearchFood
-import com.example.universityutils.features.home.data.SubjectsApi
-import com.example.universityutils.features.home.data.SubjectsRepositoryImpl
-import com.example.universityutils.features.home.domain.SubjectsRepository
-import com.example.universityutils.features.notes.data.repository.NotesRepositoryImpl
-import com.example.universityutils.features.notes.data.source.NotesDao
-import com.example.universityutils.features.notes.data.source.NotesDatabase
-import com.example.universityutils.features.notes.domain.repository.NotesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 
 @Module
 @InstallIn(SingletonComponent::class)
